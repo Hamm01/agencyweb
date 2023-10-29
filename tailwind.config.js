6366e857a5a63310cce1bb5454d2fcdb03a3c44f
Himanish kochhar <himanish.kochhar@gmail.com>
2023-10-29 16:47:54 +0530

    tailwind setups

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    './app/**/*.{js,ts,jsx,tsx}'
+  ],
+  theme: {
+    letterSpacing: {
+      tight: '-.015em'
+    },
+    extend: {
+      height: {
+        'half-screen': '50vh'
+      }
+    }
+  },
+  plugins: []
+}
