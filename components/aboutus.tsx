2da1d3400e6f536323e951b97ab6fa827a87b525
Himanish kochhar <himanish.kochhar@gmail.com>
2024-01-04 02:01:38 +0530

    about section updated

--- components/aboutus.tsx
+++ components/aboutus.tsx
@@ -3,6 +3,14 @@ import React from 'react'
 const Aboutus: React.FC = () => {
 
     return (<section className='aboutus flex flex-col bg-white py-20 text-4xl md:text-5xl'>
+        <div className="container mx-auto px-11">
+            <p className='text-4xl lg:text-4xl tracking-tight max-w-5xl leading-tight mx-auto '>
+                <strong>I will help you ship better apps, faster.</strong>
+                Created the best user experience in some of great apps worldwide.
+                I craft reliable, fast, and user-friendly applications from end to end.
+                Need a sleek UI, a powerful backend, or both? I can help you build it.
+            </p>
+        </div>
         About us
     </section>)
 }
