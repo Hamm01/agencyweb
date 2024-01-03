e1d2b870684e41779abca5af1fb76e23872c2fb0
Himanish kochhar <himanish.kochhar@gmail.com>
2024-01-03 09:03:22 +0530

    new section added

---
+++ components/aboutus.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Aboutus: React.FC = () => {
+
+    return (<section className='aboutus flex flex-col bg-white py-20 text-4xl md:text-5xl'>
+        About us
+    </section>)
+}
+export default Aboutus
