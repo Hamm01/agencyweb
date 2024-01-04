88968f2e0a7a5185f4bc6c1d9e1578b161cf9985
Himanish kochhar <himanish.kochhar@gmail.com>
2024-01-05 04:00:51 +0530

    new member component added

---
+++ components/member.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
+
+
+interface Props {
+    id: string
+    name: string
+    socialId: string
+    link: string
+}
+
+const Member: React.FC<Props> = ({ id, name, socialId, link }) => {
+
+    return (<div>slate</div>)
+}
+export default Member
