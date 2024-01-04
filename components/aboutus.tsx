420e3c33777e59a0c1111a2610ea1051f483d204
Himanish kochhar <himanish.kochhar@gmail.com>
2024-01-05 04:00:24 +0530

    about updated

--- components/aboutus.tsx
+++ components/aboutus.tsx
@@ -11,7 +11,18 @@ const Aboutus: React.FC = () => {
                 Need a sleek UI, a powerful backend, or both? I can help you build it.
             </p>
         </div>
-        About us
+        <div className="container mx-auto px-11 text-center mt-28">
+            <h2>Our Team</h2>
+            <div className="mt-2">the &ldquo;spec-ops&rdquo;</div>
+            <div className="mt-10 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-6 lg:gap-20">
+                <div>member</div>
+                <div>member</div>
+                <div>member</div>
+                <div>member</div>
+                <div>member</div>
+            </div>
+
+        </div>
     </section>)
 }
 export default Aboutus
