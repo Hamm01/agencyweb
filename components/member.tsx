5dfae320edf335b1fff1a1e6ca4e5fa91cbabe54
Himanish kochhar <himanish.kochhar@gmail.com>
2024-01-29 07:34:49 +0530

    member sections edited with link and twitter

--- components/member.tsx
+++ components/member.tsx
@@ -10,8 +10,13 @@ interface Props {
     link: string
 }
 
-const Member: React.FC<Props> = ({ id, name, socialId, link }) => {
-
-    return (<div>slate</div>)
-}
+const Member: React.FC<Props> = ({ id, name, socialId, link }) => <div>
+    <Image src={`/assets/team/webteam_face_${id}.svg`} alt={name} width={1366} height={1555} />
+    <div className='text-2xl xl:text-2xl'>{name}</div>
+    <div className=''>
+        <Link target='_blank' className='text-2xl xl:text-2xl' href={link}>
+            {socialId}
+        </Link>
+    </div>
+</div>
 export default Member
