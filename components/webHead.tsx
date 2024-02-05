b08e1b9dbb49d7c6d9298dbc6a4372b1725cddcc
Himanish kochhar <himanish.kochhar@gmail.com>
2024-02-05 09:54:12 +0530

    logo and scrool down logo loading animation done

--- components/webHead.tsx
+++ components/webHead.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 import { ScrollContext } from '../utils/scroll-observer'
 
 const AgencyHead: React.FC = () => {
-
+    const [imageLoaded, setImageLoaded] = useState(false)
     const refContainer = useRef<HTMLDivElement>(null)
     const { scrollY } = useContext(ScrollContext)
     let progress = 0
@@ -11,14 +11,17 @@ const AgencyHead: React.FC = () => {
     if (elContainer) {
         progress = Math.min(1, scrollY / elContainer.clientHeight)
     }
+    const handleImageLoaded = useCallback(() => {
+        setImageLoaded(true)
+    }, [])
     return (
-        <div ref={refContainer} className='min-h-screen sticky top-0 -z-10 flex flex-col justify-center items-center' style={{ transform: `translateY((-${progress} * 20)vh )` }}>
+        <div ref={refContainer} className='min-h-screen sticky top-0 -z-10 flex flex-col justify-center items-center' style={{ transform: `translateY(-${progress * 20}vh )` }}>
             <video autoPlay muted playsInline className='absolute w-full h-full object-cover'>
                 {/* <source src="/assets/snaphead-bg.m4v" type="video/mp4; codecs=hvc1" /> */}
                 <source src="/assets/snaphead-bg.webm" type="video/webm; codecs=vp9" />
             </video>
-            <div className="flex-grow-0 pt-10 transition-opacity  duration-1000 z-10 ">
-                <Image src="/assets/hklogowhite.png" width={128 / 2} height={114 / 2} alt="logo"></Image>
+            <div className={`flex-grow-0 pt-10 transition-opacity duration-1000 z-10 ${imageLoaded ? 'opacity-100' : 'opacity-0'} `}>
+                <Image src="/assets/hklogowhite.png" width={128 / 2} height={114 / 2} alt="logo" ></Image>
             </div>
             <div className='p-12 font-bold z-10 text-white drop-shadow-[0_5px_3px_rgba(0,0,0,0.4)] text-center flex-1 flex flex-col items-center justify-center'>
 
@@ -29,8 +32,8 @@ const AgencyHead: React.FC = () => {
                     <span>done right.</span>
                 </h2>
             </div>
-            <div className="flex-grow-0 pb-20 md:pb-10 transition-all  duration-1000 z-10 ">
-                <Image src="/assets/arrow-down.png" width={188 / 3} height={105 / 3} alt="scroll-down"></Image>
+            <div className={`flex-grow-0 pb-20 md:pb-10 transition-all  duration-1000 z-10  ${imageLoaded ? 'opacity-100' : 'opacity-0 -translate-y-20'}  `}>
+                <Image src="/assets/arrow-down.png" width={188 / 3} height={105 / 3} alt="scroll-down" onLoad={handleImageLoaded}></Image>
             </div>
         </div>
     )
