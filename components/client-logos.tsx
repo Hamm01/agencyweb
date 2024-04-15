4623227196f18e38e75599e1c9ee5fed5674fa99
Himanish kochhar <himanish.kochhar@gmail.com>
2024-04-15 09:52:36 +0530

    client logo added with pics inside it

---
+++ components/client-logos.tsx
@@ -0,0 +1,96 @@
+import React from 'react'
+import Image from 'next/image'
+import SliderContainer, { SliderItem } from './slider'
+
+
+const ClientLogos: React.FC = () => (
+
+    <>
+        <SliderContainer className="" contentWidth={1930} initialOffsetX={0}>
+            <SliderItem width={150}>
+                <Image src="/assets/trustedby/shopify.png" width={150} height={50} alt="Shopify" style={{ objectFit: 'contain' }} />
+            </SliderItem>
+            <SliderItem width={170}>
+                <Image src="/assets/trustedby/coinbase.png" width={170} height={50} alt="Coinbase" style={{ objectFit: 'contain' }} />
+            </SliderItem>
+            <SliderItem width={170}>
+                <Image src="/assets/trustedby/rainbow.png" width={170} height={50} alt="rainbow" style={{ objectFit: 'contain' }} />
+            </SliderItem>
+            <SliderItem width={90}>
+                <Image src="/assets/trustedby/redbull.png" width={90} height={50} alt="Redbull" style={{ objectFit: 'contain' }} />
+            </SliderItem>
+            <SliderItem width={200}>
+                <Image src="/assets/trustedby/walletconnect.png" width={200} height={50} alt="WalletConnect" style={{ objectFit: 'contain' }} />
+            </SliderItem>
+            <SliderItem width={150}>
+                <Image src="/assets/trustedby/nativescript.png" width={150} height={50} alt="Nativescript" style={{ objectFit: 'contain' }} />
+            </SliderItem>
+            <SliderItem width={130}>
+                <Image src="/assets/trustedby/expensify.png" width={130} height={50} alt="Expensify" style={{ objectFit: 'contain' }} />
+            </SliderItem>
+            <SliderItem width={80}>
+                <Image src="/assets/trustedby/picnic.png" width={80} height={50} alt="Picnic" style={{ objectFit: 'contain' }} />
+            </SliderItem>
+            <SliderItem width={140}>
+                <Image src="/assets/trustedby/status.png" width={140} height={50} alt="Status" style={{ objectFit: 'contain' }} />
+            </SliderItem>
+            <SliderItem width={120}>
+                <Image src="/assets/trustedby/extra.png" width={120} height={50} alt="Extra" style={{ objectFit: 'contain' }} />
+            </SliderItem>
+            <SliderItem width={110}>
+                <Image src="/assets/trustedby/stori.png" width={110} height={50} alt="Stori" style={{ objectFit: 'contain' }} />
+            </SliderItem>
+            <SliderItem width={150}>
+                <Image src="/assets/trustedby/exodus.png" width={150} height={50} alt="exodus" style={{ objectFit: 'contain' }} />
+            </SliderItem>
+            <SliderItem width={100}>
+                <Image src="/assets/trustedby/thisapp.png" width={100} height={50} alt="Thisapp" style={{ objectFit: 'contain' }} />
+            </SliderItem>
+
+        </SliderContainer>
+        <SliderContainer className="" contentWidth={1930} initialOffsetX={0}>
+            <SliderItem width={150}>
+                <Image src="/assets/trustedby/slingshot.png" width={150} height={50} alt="Slingshot" style={{ objectFit: 'contain' }} />
+            </SliderItem>
+            <SliderItem width={170}>
+                <Image src="/assets/trustedby/showtime.png" width={150} height={50} alt="Showtime" style={{ objectFit: 'contain' }} />
+            </SliderItem>
+            <SliderItem width={170}>
+                <Image src="/assets/trustedby/steakwallet.png" width={150} height={50} alt="steakwallet" style={{ objectFit: 'contain' }} />
+            </SliderItem>
+            <SliderItem width={90}>
+                <Image src="/assets/trustedby/pinkpanda.png" width={200} height={50} alt="Pinkpanda" style={{ objectFit: 'contain' }} />
+            </SliderItem>
+            <SliderItem width={200}>
+                <Image src="/assets/trustedby/litentry.png" width={130} height={50} alt="Litentry" style={{ objectFit: 'contain' }} />
+            </SliderItem>
+            <SliderItem width={150}>
+                <Image src="/assets/trustedby/raive.png" width={110} height={50} alt="Raive" style={{ objectFit: 'contain' }} />
+            </SliderItem>
+            <SliderItem width={130}>
+                <Image src="/assets/trustedby/scribeware.png" width={150} height={50} alt="Scribeware" style={{ objectFit: 'contain' }} />
+            </SliderItem>
+            <SliderItem width={80}>
+                <Image src="/assets/trustedby/snapcalorie.png" width={120} height={50} alt="Snapcalorie" style={{ objectFit: 'contain' }} />
+            </SliderItem>
+            <SliderItem width={140}>
+                <Image src="/assets/trustedby/push.png" width={70} height={50} alt="Push" style={{ objectFit: 'contain' }} />
+            </SliderItem>
+            <SliderItem width={120}>
+                <Image src="/assets/trustedby/tocsen.png" width={110} height={50} alt="Toscen" style={{ objectFit: 'contain' }} />
+            </SliderItem>
+            <SliderItem width={110}>
+                <Image src="/assets/trustedby/audubon.png" width={150} height={50} alt="Audubon" style={{ objectFit: 'contain' }} />
+            </SliderItem>
+            <SliderItem width={150}>
+                <Image src="/assets/trustedby/steddy.png" width={135} height={50} alt="Steddy" style={{ objectFit: 'contain' }} />
+            </SliderItem>
+            <SliderItem width={100}>
+                <Image src="/assets/trustedby/candid.png" width={180} height={50} alt="Candid" style={{ objectFit: 'contain' }} />
+            </SliderItem>
+
+        </SliderContainer>
+    </>
+)
+
+export default ClientLogos
